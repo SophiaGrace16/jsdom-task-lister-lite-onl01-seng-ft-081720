c57a6830334bd4e14f54728ccc1cc94977b2b0ce
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-24 11:21:03 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const input = document.getElementById('create-task-form');
+
+  input.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let taskElement=document.getElementById("new-task-description");
+    let newTask = taskElement.value;
+    // console.log(newTask);
+
+    let newItem = document.createElement('li');
+    newItem.innerText = newTask;
+
+    let list=document.getElementById("tasks");
+    list.appendChild(newItem);
+    taskElement.value = '';
+  }
+);
+
 });
